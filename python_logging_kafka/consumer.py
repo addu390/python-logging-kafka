8ea3b11de5bd8f260fcda19361aed74fba0c3e59
Adesh Nalpet <390.adesh@gmail.com>
2020-10-04 23:51:00 +0530

    Add a consumer file for reference - Producer

--- python_logging_kafka/consumer.py
+++ python_logging_kafka/consumer.py
@@ -1,5 +1,9 @@
 from kafka import KafkaConsumer
 
+"""
+Python stand-alone consumer for testing
+logs produced from Kafka Producer.
+"""
 consumer = KafkaConsumer('pylog')
 for message in consumer:
     print(message)
